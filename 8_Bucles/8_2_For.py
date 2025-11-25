9233aba11e720071a12df16418d1f20d2495c12f
dcsoft-ux <cdazaparra@gmail.com>
2025-11-25 15:29:48 -0500

    Update 8_2_For.py

--- 8_Bucles/8_2_For.py
+++ 8_Bucles/8_2_For.py
@@ -1,16 +1,4 @@
-#➡️ Iterar sobre cada letra de la palabra "Abecedario"
-#➡️ Ejemplo de un ciclo for en Python
-#➡️ Imprime cada letra de la palabra "Abecedario" utilizando un ciclo for
-#➡️ La variable letra toma el valor de cada carácter en la palabra durante cada iteración del ciclo
-#➡️ El ciclo for se repite hasta que se hayan impreso todas las letras de la palabra
-#➡️ Inicialización de la variable palabra
-palabra = "Abecedario"
-#➡️ Ciclo for para iterar sobre cada letra en la palabra
-#➡️ Imprime cada letra de la palabra
-#➡️ Al finalizar el ciclo, todas las letras de la palabra habrán sido impresas
-for letra in palabra:
-    #➡️ Imprime la letra actual
-    #➡️ La variable letra toma el valor de cada carácter en la palabra durante cada iteración
-    #➡️ El ciclo se repite hasta que se hayan impreso todas las letras de la palabra
-    #➡️ Al finalizar el ciclo, todas las letras de la palabra habrán sido impresas
-    print(letra)
+numero = int(input("Ingrese un número: "))
+for i in range(0, 10):
+    multiplicacion = numero * i
+    print (multiplicacion)
