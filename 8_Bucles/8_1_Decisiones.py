35064b55f55d96c586310921df2f422989d60f50
Rodolfo Guerrero Cisterna <140962839+Ragnarwof94@users.noreply.github.com>
2025-11-25 13:19:21 -0300

    Se añade carpeta y nuevo archivos.

---
+++ 8_Bucles/8_1_Decisiones.py
@@ -0,0 +1,9 @@
+mascosta = input ("ingrese su mascota: ")
+if mascosta == "gato":
+    print("es un gato")
+elif mascosta == "perro":
+    print("es un perro")
+elif mascosta == "gallina":
+    print("es una gallina")
+else:
+    print("No es ninguna de las opciones")
